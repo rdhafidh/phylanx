ecb50f0952715a384655aa131e2f27c435568149
weilewei <lokwei9@gmail.com>
2018-07-09 12:55:20 -0500

    adding dictionary to phylanx

---
+++ src/execution_tree/primitives/dictionary.cpp
@@ -0,0 +1,24 @@
+//  Copyright (c) 2017-2018 Weile Wei
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <phylanx/config.hpp>
+#include <phylanx/execution_tree/ir/dictionary.hpp>
+
+#include <hpx/include/lcos.hpp>
+#include <hpx/include/naming.hpp>
+#include <hpx/include/util.hpp>
+#include <hpx/throw_exception.hpp>
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace phylanx { namespace execution_tree { namespace primitives
+{
+
+
+}}}
